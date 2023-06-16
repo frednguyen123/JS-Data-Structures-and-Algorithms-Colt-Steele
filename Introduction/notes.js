875c9f5763d0e6a0e6c078e470bccfaf0112d93c
frednguyen123 <107221212+frednguyen123@users.noreply.github.com>
2023-06-15 20:43:32 -0700

    git commit

---
+++ Introduction/notes.js
@@ -0,0 +1,68 @@
+/**
+ * Curriculum Walkthrough
+ */
+
+/**
+ * BIG O NOTATION
+ * Describing perforamnce and efficiency of code
+ * 
+ * ANALYZING PERFORMANCE OF ARRAY AND OBJECTS
+ * Use Big O and apply it to built in datastructures
+ * 
+ * PROBLEM SOLVING APPROACH
+ * How to confront tough problems
+ * Framework to approach problem
+ * 
+ * PROBLEM SOLVING PATTERNS
+ * Patterns to learn specific architecture of your code
+ * 
+ * RECURSION
+ * Challenges and used in other algorithms
+ * 
+ * SEARCHING ALGORITHMS
+ * Ex: Binary Search
+ * 
+ * SORTING ALGORTIHMS
+ * 6+ Sorting Algorithms
+ * 
+ * BUBBLE SORT/SELECTION SORT/ INSERTION SORT
+ * Beginner/Intermediate sorting
+ * 
+ * MERGE SORT / QUICK SORT
+ * Involve Big O and Recursion
+ * 
+ * RADIX SORT
+ * Fast and special situations
+ * 
+ * INTRO TO DATA STRUCTURES
+ * Talk about ES2015 class syntax
+ * Which data structure for each situation
+ * 
+ * SINGLY LINKED LIST / DOUBLY LINKED LIST
+ * 
+ * STACKS AND QUEUES
+ * Common use
+ * 
+ * BINARY SEARCH TREES
+ * 
+ * TREE TRAVERSAL
+ * 
+ * BINARY HEAPS
+ * 
+ * HASH TABLES
+ * Understanding how JS Objects work
+ * Hashing Algorithms
+ * 
+ * GRAPHS
+ * Model GPS
+ * Mapping
+ * How to represent a map
+ * 
+ * GRAPH TRAVERSAL
+ * 
+ * DIJKSTRAS ALGORITHMS
+ * Finding shortest path between two nodes on a graph
+ * 
+ * 
+ */
+
