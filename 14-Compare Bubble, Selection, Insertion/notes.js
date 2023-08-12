7155a795ea61b0c60b9cf843d385a67614c78e9c
frednguyen123 <107221212+frednguyen123@users.noreply.github.com>
2023-08-11 21:31:56 -0700

    git commit

---
+++ 14-Compare Bubble, Selection, Insertion/notes.js
@@ -0,0 +1,32 @@
+/**
+ * Comparing Bubble, Selection, & Insertion Sort
+ */
+
+/**
+ * BIG O OF SORTING ALGORITHMS
+ * 
+ * BUBBLESORT
+ * Best: O(N)
+ * Average: O(N^2)
+ * Worst: O(N^2)
+ *
+ * SELECTION SORT
+ * Best: O(N^2)
+ * Average: O(N^2)
+ * Worst: O(N^2)
+ * 
+ * INSERTION SORT
+ * Best: O(N)
+ * Average: O(N^2)
+ * Worst: O(N^2)
+ * 
+ * SPACE COMPLEXITY FOR ALL: O(1)
+ * 
+ * RECAP
+ * Sorting is fundamental
+ * Bubble sort, selection sort, and insertion sort are all roughly equivalent.
+ * All have average time complexities that are quadratic
+ * We can do better ...but we need more complex algorithms!
+ * 
+ */
+
